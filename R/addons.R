04c8205c63c5a3c17a9a0e583df76d7a88b41a1a
Mark Heckmann <heckmann.mark@gmail.com>
2015-05-11 18:35:30 +0200

    new feature wdReplaceTextByText

--- R/addons.R
+++ R/addons.R
@@ -153,3 +153,33 @@ wdAddImageCaption <- function(i, ishp=NULL, title="", sep=":",
                       Title=title,
                       Position=1)        # 1=below
 }
+
+
+#' Searches and replaces a string
+#' 
+#' This function replaces a piece of text with another text.
+#' 
+#' @param find A string.
+#' @param text Replacement text.
+#' @return Invisibly returns a pointer to the range object.
+#' @export
+#' @section TODO: testing
+#' 
+wdReplaceTextByText <- function(find = "", text, wdapp = .R2wd) 
+{
+  success <- wdSearchString(find, wdapp=wdapp)    # search string in document and select
+  
+  # if text was not found
+  if (!success) {
+    if (warn)
+      warning("The find text was not found in document", call. = FALSE)
+    return(FALSE)
+  } 
+  
+  # delete and replace
+  r <- wdapp[["Selection"]][["Range"]]            # get range of selection
+  r[["Text"]] <- text
+  invisible(r)
+}
+
+
